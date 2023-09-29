6902d6324ac713d3164b2a1fc464d04384e31e21
TanaySrivas <tanaysriofficial@gmail.com>
2023-09-30 02:15:37 +0530

    Game working

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,19 +1,36 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
+
 fn main() {
     println!("Guess the number !");
 
     let secret_num = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is: {secret_num}");
-    
-    println!("Please input your guess");
-    
-    let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read input");
+    loop{
+        println!("Please input your guess");
+        
+        let mut guess = String::new();
 
-    println!("You guessed: {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read input");
 
+        println!("You guessed: {guess}");
+
+        let guess: u32 = guess.trim().parse().expect("Please enter a number!");
+
+        match guess.cmp(&secret_num){
+            //cmp compares and returns a variant of "Ordering" type (Less, Greater or Equal)
+            //match is like a switch case that on receiving the variant goes
+            //and matches against the keys or arms in Rust and executes the matching one
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
