b5ed2eb70cc232d4376173502c1c57f1cbb645d2
TanaySrivas <tanaysriofficial@gmail.com>
2023-09-30 04:47:26 +0530

    Shadowing checked

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     //immutable
-    let x = 6;
-    println!("value of x: {x}");
+    let k = 6;
+    println!("value of x: {k}");
 
-    // x = 5;//this line will throw an error
+    // k = 5;//this line will throw an error
     // println!("value of x: {x}");
 
     //mutable
@@ -12,4 +12,19 @@ fn main() {
 
     x = 6;
     println!("value of x: {x}");
+
+    //Shadowing
+    let y = 10;
+
+    let y = y + 1;
+    {
+        let y = y * 2;
+        println!("Value of y from inner scope is: {y}");
+    }
+    println!("Value of y: {y}");
+
+    //we can also change type as it's basically a new initialization
+    let spaces = "    ";
+    let spaces = spaces.len();
+    println!("Value of spaces: {spaces}");
 }
