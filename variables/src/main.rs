a5a89e92a9146383c6f3374c9f11502e2f630524
TanaySrivas <tanaysriofficial@gmail.com>
2023-09-30 04:40:52 +0530

    Mutable variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,15 @@
 fn main() {
+    //immutable
     let x = 6;
     println!("value of x: {x}");
 
-    x = 5;//this line will throw an error
+    // x = 5;//this line will throw an error
+    // println!("value of x: {x}");
+
+    //mutable
+    let mut x = 5;
+    println!("value of x: {x}");
+
+    x = 6;
     println!("value of x: {x}");
 }
---
+++ variables/target/debug/deps/variables-e18d4ea05ac0736c.d
@@ -0,0 +1,5 @@
+T:\EmpoweredCoder\Tech\Rust-Basics\variables\target\debug\deps\variables-e18d4ea05ac0736c.rmeta: src\main.rs
+
+T:\EmpoweredCoder\Tech\Rust-Basics\variables\target\debug\deps\variables-e18d4ea05ac0736c.d: src\main.rs
+
+src\main.rs:
